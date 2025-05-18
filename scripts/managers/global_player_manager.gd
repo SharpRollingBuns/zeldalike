353801f89cd49288a2066c619dc0b80033d7ca46
Grickalika <lemonusyttw@gmail.com>
2025-05-18 14:59:10 +0900

    spawner & HUD

--- scripts/managers/global_player_manager.gd
+++ scripts/managers/global_player_manager.gd
@@ -1,4 +1,31 @@
 extends Node
 
+const PLAYER = preload("res://scenes/entities/Player/player.tscn")
 
 var player : Player
+var player_spawner : bool = false 
+
+
+func _ready() -> void:
+	add_player_instance()
+	await get_tree().create_timer(0.2).timeout
+	player_spawner = true
+
+
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
+
+
+func set_player_position(_new_pos: Vector2) -> void:
+	player.global_position = _new_pos
+
+
+func set_as_parent(_p:Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	_p.add_child(player)
+
+
+func unparent_player(_p:Node2D) -> void:
+	_p.remove_child(player)
